2c4df8726fd28049ac3615ee2283d92c61a384af
Saya-such <work@such-web.com>
2025-07-29 17:30:21 +0900

    feat: Conceptセクションのキャッチコピーの表示・非表示アニメーションの追加

--- src/components/Concept/catchAnimation.ts
+++ src/components/Concept/catchAnimation.ts
@@ -1,5 +1,36 @@
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-const chatchAnimation = () => {};
+const chatchAnimation = () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const trigger = document.getElementById("concept-catch");
+  const conceptCatchEl = trigger?.querySelector("span");
+  const catchSize = conceptCatchEl?.clientHeight;
+  const conceptFixed = document.getElementById("concept-fixed");
+
+  if (catchSize && conceptCatchEl && trigger) {
+    const offset = Number(catchSize + 50);
+    console.log(offset);
+
+    gsap.timeline({
+      scrollTrigger: {
+        trigger,
+        start: `top -${offset}px`,
+        scrub: true,
+        onEnter: () => {
+          trigger.classList.remove("fixed");
+          conceptFixed?.classList.remove("h-lvh");
+          conceptCatchEl.classList.add("invisible");
+        },
+        onLeaveBack: () => {
+          trigger.classList.add("fixed");
+          conceptFixed?.classList.add("h-lvh");
+          conceptCatchEl.classList.remove("invisible");
+        },
+      },
+    });
+  }
+};
 
 export default chatchAnimation;
