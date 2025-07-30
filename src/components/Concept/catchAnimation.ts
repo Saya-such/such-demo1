27faab3c1d2cac064087bedade871b3c35e99b44
Saya-such <work@such-web.com>
2025-07-30 10:04:40 +0900

    feat: コンセプトセクションのスクロールアニメーション

--- src/components/Concept/catchAnimation.ts
+++ src/components/Concept/catchAnimation.ts
@@ -8,12 +8,16 @@ const chatchAnimation = () => {
   const conceptCatchEl = trigger?.querySelector("span");
   const catchSize = conceptCatchEl?.clientHeight;
   const conceptFixed = document.getElementById("concept-fixed");
+  const conceptMain = document.getElementById("concept-main");
+  const scrollCatchEl = document
+    .getElementById("catch-scroll")
+    ?.querySelector("span");
 
-  if (catchSize && conceptCatchEl && trigger) {
+  if (catchSize && conceptCatchEl && trigger && scrollCatchEl) {
     const offset = Number(catchSize + 50);
     console.log(offset);
 
-    gsap.timeline({
+    const tl = gsap.timeline({
       scrollTrigger: {
         trigger,
         start: `top -${offset}px`,
@@ -21,15 +25,38 @@ const chatchAnimation = () => {
         onEnter: () => {
           trigger.classList.remove("fixed");
           conceptFixed?.classList.remove("h-lvh");
-          conceptCatchEl.classList.add("invisible");
+          conceptFixed?.classList.add("hidden");
+          conceptMain?.classList.remove("opacity-0");
+          conceptMain?.classList.add("opacity-100");
+          scrollCatchEl.classList.add("scroll");
         },
         onLeaveBack: () => {
           trigger.classList.add("fixed");
           conceptFixed?.classList.add("h-lvh");
-          conceptCatchEl.classList.remove("invisible");
+          conceptFixed?.classList.remove("hidden");
+          conceptMain?.classList.remove("opacity-100");
+          conceptMain?.classList.add("opacity-0");
+          scrollCatchEl.classList.remove("scroll");
         },
       },
     });
+
+    tl.fromTo(
+      scrollCatchEl,
+      {
+        skewY: 0,
+        scaleX: 1.0,
+        scaleY: 1.0,
+        rotate: 0,
+      },
+      {
+        skewY: 12,
+        scaleX: 1.05,
+        scaleY: 1.6,
+        rotate: -12,
+        duration: 3.0,
+      },
+    );
   }
 };
 
