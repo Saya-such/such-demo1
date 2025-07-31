5368a148faeb5171300832404416665ef24a2c77
Saya-such <work@such-web.com>
2025-07-31 16:09:09 +0900

    feat: Worksのテキストアニメーション

--- src/components/Works/initWorks.ts
+++ src/components/Works/initWorks.ts
@@ -1,11 +1,14 @@
 import startGalleryLoop from "./startGalleryLoop";
+import startTextLoop from "./startTextLoop";
 
 const initWorks = () => {
   const galleryList = document.getElementById("works-list");
+  const loopText = document.getElementById("loop-text");
 
-  if (!galleryList) return;
+  if (!galleryList || !loopText) return;
 
   startGalleryLoop(galleryList);
+  startTextLoop(loopText);
 };
 
 export default initWorks;
--- src/components/Works/startTextLoop.ts
+++ src/components/Works/startTextLoop.ts
@@ -1,3 +1,14 @@
-const startTextLoop = () => {};
+import { gsap } from "gsap";
+
+const startTextLoop = (loopText: HTMLElement) => {
+  const totalWidth = loopText.scrollWidth / 2;
+
+  return gsap.timeline().to(loopText, {
+    x: -totalWidth,
+    duration: 20,
+    ease: "none",
+    repeat: -1,
+  });
+};
 
 export default startTextLoop;
