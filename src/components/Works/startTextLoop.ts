b637f2b238993b59abfc13b47ce8ce01892162e6
Saya-such <work@such-web.com>
2025-08-01 15:44:29 +0900

    fix: Worksのテキストアニメーションのフォントサイズ調整とコンセプトとの余白調整

--- src/components/Works/startTextLoop.ts
+++ src/components/Works/startTextLoop.ts
@@ -2,13 +2,29 @@ import { gsap } from "gsap";
 
 const startTextLoop = (loopText: HTMLElement) => {
   const totalWidth = loopText.scrollWidth / 2;
+  let mm = gsap.matchMedia();
 
-  return gsap.timeline().to(loopText, {
-    x: -totalWidth,
-    duration: 20,
-    ease: "none",
-    repeat: -1,
+  const tl = gsap.timeline();
+
+  mm.add("(max-width: 1023px)", () => {
+    tl.to(loopText, {
+      x: -totalWidth,
+      duration: 20,
+      ease: "none",
+      repeat: -1,
+    });
   });
+
+  mm.add("(min-width: 1024px)", () => {
+    tl.to(loopText, {
+      x: -totalWidth,
+      duration: 30,
+      ease: "none",
+      repeat: -1,
+    });
+  });
+
+  return tl;
 };
 
 export default startTextLoop;
