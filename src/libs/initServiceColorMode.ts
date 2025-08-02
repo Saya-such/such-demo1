b3469f79a1013827830c32cb5e18b3506fa57fd7
Saya-such <work@such-web.com>
2025-08-02 13:27:23 +0900

    feat: Serviceコンポーネントの表示でdatasetの値を変更する関数をpages/index.astroに追加

---
+++ src/libs/initServiceColorMode.ts
@@ -0,0 +1,20 @@
+const initServiceColorMode = () => {
+  const sectionEl = document.getElementById("service");
+  const rootEl = document.getElementById("page-root");
+
+  if (!sectionEl || !rootEl) return;
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(
+      (entry) => {
+        const mode = entry.isIntersecting ? "dark" : "light";
+        rootEl.dataset.color = mode;
+      },
+      { threshold: 0 },
+    );
+  });
+
+  observer.observe(sectionEl);
+};
+
+export default initServiceColorMode;
---
+++ src/types/ColorMode.ts
@@ -0,0 +1,5 @@
+type ColorMode = {
+  color: "light" | "dark";
+};
+
+export type { ColorMode };
