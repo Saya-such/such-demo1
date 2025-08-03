4e0eadac83b54017d76cf0121995603a9e30a6d9
Saya-such <work@such-web.com>
2025-08-04 08:03:07 +0900

    fix: 縦長の時にnewsセクションのpadding-topを設定せず、serviceセクションのpadding-bottomを設定

--- src/libs/initColorMode.ts
+++ src/libs/initColorMode.ts
@@ -13,8 +13,17 @@ const initColorMode = () => {
         } else {
           if (entry.intersectionRatio <= 0.3 || !entry.isIntersecting) {
             rootEl.dataset.color = "light";
-          } else if (window.scrollY < serviceEl.getBoundingClientRect().top) {
+            console.log(
+              "called serviceObserver: entry.intersectionRatio <= 0.3 || !entry.isIntersecting",
+            );
+          } else if (
+            window.scrollY <
+            serviceEl.getBoundingClientRect().top + window.scrollY
+          ) {
             rootEl.dataset.color = "light";
+            console.log(
+              "called serviceObserver: window.scrollY < serviceEl.getBoundingClientRect().top",
+            );
           }
         }
       },
@@ -30,8 +39,10 @@ const initColorMode = () => {
       (entry) => {
         if (entry.isIntersecting) {
           rootEl.dataset.color = "light";
+          console.log("called newsCbserver: entry.isIntersecting");
         } else if (entry.intersectionRatio > 0.3) {
           rootEl.dataset.color = "dark";
+          console.log("called newsObserver: entry.intersectionRatio > 0.3");
         }
       },
       { threshold: [0, 0.3] },
