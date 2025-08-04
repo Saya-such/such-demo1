5b3240aad7f6e333cf6913950a86a6242c114e3d
Saya-such <work@such-web.com>
2025-08-04 09:07:01 +0900

    fix: 不要なIntersectionObserverの条件分岐・console.log削除・

--- src/libs/reload.ts
+++ src/libs/reload.ts
@@ -1,17 +1,14 @@
 const reload = () => {
   const rootEl = document.getElementById("page-root");
   let initialWidth = window.innerWidth;
-  console.log("reloaded");
 
   if (rootEl) {
     rootEl.dataset.color = "light";
-    console.log("call reloaded set light");
   }
 
   window.addEventListener("resize", () => {
     if (initialWidth !== window.innerWidth) {
       window.location.reload();
-      console.log("resize");
     }
   });
 };
