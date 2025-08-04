66a2a68f4bae7b43f83baf74047224b341488a3c
Saya-such <work@such-web.com>
2025-08-04 15:40:35 +0900

    fix: IntersectionObserverのrootMarginとthreshouldの調整(Serviceセクション検知で背景色変化)

--- src/libs/initColorMode.ts
+++ src/libs/initColorMode.ts
@@ -5,38 +5,35 @@ const initColorMode = () => {
 
   if (!serviceEl || !newsEl || !rootEl) return;
 
-  const serviceObserver = new IntersectionObserver((entries) => {
-    entries.forEach(
-      (entry) => {
-        if (entry.isIntersecting) {
-          rootEl.dataset.color = "dark";
-        } else {
-          if (entry.intersectionRatio <= 0.3 || !entry.isIntersecting) {
+  const isSp = window.matchMedia("(max-width: 799px)").matches;
+
+  const isPortrait = window.matchMedia(
+    "(orientation: portrait) and (min-width: 800px)",
+  ).matches;
+
+  const serviceObserver = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (!isPortrait) {
+          if (entry.isIntersecting) {
+            if ((entry.intersectionRatio >= 0, entry.intersectionRatio < 0.3)) {
+              rootEl.dataset.color = "light";
+            } else {
+              rootEl.dataset.color = "dark";
+            }
+          } else {
             rootEl.dataset.color = "light";
           }
         }
-      },
-
-      { threshold: [0, 0.3] },
-    );
-  });
+      });
+    },
+    {
+      rootMargin: "0px 0px 50% 0px",
+      threshold: [0, 0.3],
+    },
+  );
 
   serviceObserver.observe(serviceEl);
-
-  const newsObserver = new IntersectionObserver((entries) => {
-    entries.forEach(
-      (entry) => {
-        if (entry.isIntersecting) {
-          rootEl.dataset.color = "light";
-        } else if (entry.intersectionRatio > 0.3) {
-          rootEl.dataset.color = "dark";
-        }
-      },
-      { threshold: [0, 0.3] },
-    );
-  });
-
-  newsObserver.observe(newsEl);
 };
 
 export default initColorMode;
