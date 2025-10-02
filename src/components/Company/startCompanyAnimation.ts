1e0628d9954f038b52d0e000a5d984536b429afe
Saya-such <work@such-web.com>
2025-10-02 13:43:54 +0900

    fix: companyセクションの高さを修正

--- src/components/Company/startCompanyAnimation.ts
+++ src/components/Company/startCompanyAnimation.ts
@@ -4,9 +4,10 @@ import createCompanyZoomTimeline from "./createCompanyZoomTimeline";
 /**
  * companyセクション用のアニメーションのTimelineをまとめて生成し、実行する。
  */
-const startCompanyAnimation = (): void => {
-  createCompanyPinTimeline();
+const startCompanyAnimation = () => {
+  //順序が重要なので注意！（zoom → pinの順）
   createCompanyZoomTimeline();
+  createCompanyPinTimeline();
 };
 
 export default startCompanyAnimation;
